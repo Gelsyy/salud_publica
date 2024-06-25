b6bf8e8ae6c08289f3a5c36ed80fa8a0edebcefd
oslianyabel <oslianyabel@gmail.com>
2024-06-25 16:05:43 -0400

    arreglos Osliani

--- salud_publica/app/forms.py
+++ salud_publica/app/forms.py
@@ -3,10 +3,10 @@ from .models import Inventario
 
 class InventarioForm(forms.ModelForm):
     class Meta:
-        model =Inventario
-        fields=[ 'lote', 'id_insumo', 'id_hospital', 'cantidad_entrada', 'fecha_vencimiento', 'fecha_entrada']
+        model = Inventario
+        fields=['lote', 'id_insumo', 'id_hospital', 'cantidad_entrada', 'fecha_vencimiento', 'fecha_entrada']
 
 class EditarForm(forms.ModelForm):
     class Meta:
-        model =Inventario
-        fields=[ 'lote', 'id_insumo', 'id_hospital', 'cantidad_entrada', 'cantidad_salida', 'fecha_vencimiento', 'fecha_entrada']
+        model = Inventario
+        fields=['lote', 'id_insumo', 'id_hospital', 'cantidad_entrada', 'cantidad_salida', 'fecha_vencimiento', 'fecha_entrada']
